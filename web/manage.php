945d56e87943eefee07ff0be5b8b608e0ae1971b
diegoschneider <diegoschneider.96@gmail.com>
2014-06-24 03:15:10 -0300

    Agregado header a manage.php

--- web/manage.php
+++ web/manage.php
@@ -4,13 +4,15 @@ require("./src/script/functions.php");
 
 <html>
 <head>
-	<?php style_head(); ?>
+    <?php style_head(); ?>
 </head>
 <body>
+
+<?php style_header(); ?>
+
 <div id="content">
-
 	<?php echo $_SESSION['user']->name ?>
-
 </div>
+
 </body>
 </html>
