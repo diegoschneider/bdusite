39c931649849a42b71be0610eb30f45a44d0d73b
Diego Schneider <diegoschneider.96@gmail.com>
2014-09-03 21:45:28 -0300

    Sincronización feat/diseno -> develop | Ahora a trabajar bien :3

--- web/pull.php
+++ web/pull.php
@@ -1,8 +1,9 @@
 <?php
-echo "&gt git pull";
-$ret = nl2br(shell_exec("cd \"C:\Documents and Settings\Administrador\GitHub\bdusite\" & git pull 2>&1"));
+echo "&gt git pull & checkout<br>";
+$ret = nl2br(shell_exec("cd \"C:\\Documents and Settings\\Administrador\\GitHub\\bdusite\\\" & git checkout ".$_GET["checkout"]." & git pull 2>&1"));
 echo $ret."<br>";
 
+sleep(5);
 echo "&gt copyser";
 $ret = nl2br(shell_exec("copyser 2>&1"));
 echo $ret."<br>";
