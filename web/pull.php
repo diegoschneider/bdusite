2085a5dc1c68ffd99968e81d31a429b09727cdac
Diego Schneider <diegoschneider.96@gmail.com>
2014-09-03 21:25:53 -0300

    pull.php finalizado

--- web/pull.php
+++ web/pull.php
@@ -1,9 +1,11 @@
 <?php
-echo "&gt git pull<br>";
-$ret = exec("../git pull");
+echo "&gt git pull";
+$ret = nl2br(shell_exec("cd \"C:\Documents and Settings\Administrador\GitHub\bdusite\" & git pull 2>&1"));
 echo $ret."<br>";
-echo "&gt copyser<br>";
-$ret = exec("copyser");
+
+echo "&gt copyser";
+$ret = nl2br(shell_exec("copyser 2>&1"));
 echo $ret."<br>";
+
 echo "Done! :3";
 ?>
