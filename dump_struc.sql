651f7b5b7a2f791dffdaa891feef8e6d1f209c84
Diego <diegoschneider.96@gmail.com>
2014-10-03 17:35:27 -0300

    Agregados dump

--- dump_struc.sql
+++ dump_struc.sql
@@ -16,6 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `a_contacto`
+--
+
+DROP TABLE IF EXISTS `a_contacto`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `a_contacto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `userid` int(11) DEFAULT NULL,
+  `subject` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `message` varchar(500) COLLATE utf8_bin NOT NULL,
+  `solved` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `timestamp` (`timestamp`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `a_contacto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `alumnos`
 --
 
@@ -76,7 +97,8 @@ CREATE TABLE `alumnos` (
   KEY `barrio` (`barrio`),
   KEY `localidad` (`localidad`),
   KEY `escproc` (`escproc`),
-  KEY `condinscrip` (`condinscrip`)
+  KEY `condinscrip` (`condinscrip`),
+  CONSTRAINT `alumnos_ibfk_1` FOREIGN KEY (`turno`) REFERENCES `turnos` (`cod`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
 
@@ -419,7 +441,7 @@ CREATE TABLE `logins` (
   PRIMARY KEY (`id`),
   KEY `ip` (`ip`),
   KEY `userid` (`userid`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -656,7 +678,7 @@ CREATE TABLE `users` (
   `perm` int(11) NOT NULL DEFAULT '0',
   `loginfails` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -682,4 +704,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-09-12 20:54:09
+-- Dump completed on 2014-10-03 20:34:33
